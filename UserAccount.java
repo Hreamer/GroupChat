af24da8a02a7c42e9134bfbc449b34299eb8a3bf
SergioHernande <70532257+SergioHernande@users.noreply.github.com>
2020-11-28 16:43:43 -0500

    Update UserAccount.java

--- UserAccount.java
+++ UserAccount.java
@@ -1,14 +1,37 @@
+import java.awt.*;
+import java.util.ArrayList;
+
 public class UserAccount {
     private String userName;
     private String password;
+    private ArrayList<Conversation> co;
+    private Conversation have;
 
-    public UserAccount(String userName, String password) {
+    public UserAccount(String userName, String password, ArrayList<Conversation> co) {
+        this.co = new ArrayList<>();
         this.userName = userName;
         this.password = password;
     }
 
     public String getUserName() {
         return this.userName;
+    }
+    public ArrayList<Conversation> getConversations() {
+        return co;
+    }
+
+    public Conversation findConversation(String title) {
+        for (int i = 0; i < co.size(); i++) {
+            if (co.get(i).getTitle().equals(title)) {
+                have = co.get(i);
+                break;
+            }
+        }
+        return have;
+    }
+
+    public void addCo(Conversation conversation) {
+        co.add(conversation);
     }
 
     public String getPassword() {
@@ -22,7 +45,7 @@ public class UserAccount {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     @Override
     public String toString() {
         return "UserAccount{" +
