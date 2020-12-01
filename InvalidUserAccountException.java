2a87ef1f50c256f973f849d6d74f2ec33728468f
Hreamer <hreamer@purdue.edu>
2020-12-01 17:55:01 -0500

    Delete InvalidUserAccountException.java
    No longer need this exception

--- InvalidUserAccountException.java
+++
@@ -1,9 +0,0 @@
-public class InvalidUserAccountException extends Exception {
-    public InvalidUserAccountException(String message) {
-        super(message);
-    }
-
-    public InvalidUserAccountException() {
-        super();
-    }
-}
