317ed9bc363da6bea58a34b84b43897f395a657c
hreamer <reamer.hudson@gmail.com>
2020-12-03 17:35:17 -0500

    Updated ServerThread.java

--- ServerThread.java
+++ ServerThread.java
@@ -292,7 +292,7 @@ public class ServerThread extends Thread{
                         FileReader fr1 = new FileReader(f1);
                         BufferedReader br1 = new BufferedReader(fr1);
 
-                        allConversations += "Conversation - "  + convoName + " +=- ";
+                        allConversations += "Conversation --- "  + convoName + " +=- ";
 
                         String transcript = br1.readLine();
                         while(transcript != null) {
